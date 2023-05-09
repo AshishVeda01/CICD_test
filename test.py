8dc53874590470417b93597c481662e82a647081
Ashish Veda <ashish.veda@tigeranalytics.local>
2023-05-09 19:54:46 +0530

    testing CICD2

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from sample import sum
 class CheckSum(unittest.TestCase):
     def test_list_int(self):
         print("testing sample code")
+        print("test2")  
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
