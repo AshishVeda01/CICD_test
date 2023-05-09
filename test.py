0bb2656e85191c1a95d7434dbec0fc86d80a1473
Ashish Veda <ashish.veda@tigeranalytics.local>
2023-05-09 19:41:35 +0530

    flake8 cleanup

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-import unittest  
-  
+import unittest
+
 from sample import sum  
-  
-  
-class CheckSum(unittest.TestCase):  
-    def test_list_int(self):  
-  
-        data = [1, 2, 3]  
-        result = sum(data)  
-        self.assertEqual(result, 6)      
-  
-if __name__ == '__main__':  
-    unittest.main()  
+
+
+class CheckSum(unittest.TestCase):
+    def test_list_int(self):
+
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
